695a5028ee612ea49340dc235f7e41b9580c538b
Ryan Ngo <rayango92@gmail.com>
2018-03-01 15:21:42 -0800

    Add logic for verifying game winner

--- app.js
+++ app.js
@@ -13,6 +13,42 @@ let ticTacToe = {
     let playerThatGoesFirst = Math.floor(Math.random() * 2);
     this.playerForCurrentTurn = playerThatGoesFirst;
     this.playerForTurnOne = playerThatGoesFirst;
+  },
+  checkForWinner: function(mostRecentMove, piece) {
+    if (this.checkHorizontal(mostRecentMove, piece) || this.checkVertical(mostRecentMove, piece) || this.checkLeftToBottom(mostRecentMove, piece) || this.checkRightToBotom(mostRecentMove, piece)) {
+      return true;
+    }
+    return false;
+  },
+  checkHorizontal: function(mostRecentMove, piece) {
+    if (mostRecentMove <= 2 && board[0] === piece && board[1] === piece && board[2] === piece) {
+      return true;
+    } else if (mostRecentMove <= 5 && board[3] === piece && board[4] === piece && board[5] === piece) {
+      return true;
+    } else if (mostRecentMove <= 8 && board[6] === piece && board[7] === piece && board[8] === piece) {
+      return true;
+    }
+    return false;
+  },
+  checkVertical: function(mostRecentMove, piece) {
+    let columnTop = mostRecentMove;
+    while (columnTop > 2) {
+      columnTop -= 3;
+    }
+    if (columnTop === 0 && board[0] === piece && board[3] === piece && board[6] === piece) {
+      return true;
+    } else if (columnTop === 1 && board[1] === piece && board[4] === piece && board[7] === piece) {
+      return true;
+    } else if (columnTop === 2 && board[2] === piece && board[5] === piece && board[8] === piece) {
+      return true;
+    }
+    return false;
+  },
+  checkLeftToBottom: function(mostRecentMove, piece) {
+    return board[0] === piece && board[4] === piece && board[8] === piece;
+  },
+  checkRightToBotom: function(mostRecentMove, piece) {
+    return board[2] === piece && board[4] === piece && board[6] === piece;
   }
 };
 
@@ -20,9 +56,12 @@ let handlers = {
   addPiece: function(cell) {
     let index = this.getIndexOfCell(cell.id);
     if (this.verifyMove(index)) {
-      ticTacToe.board[index] = cell.innerHTML = ticTacToe.playerForCurrentTurn ? ticTacToe.piece['1'] : ticTacToe.piece['0'];
-      ticTacToe.turnNumber++;
-      ticTacToe.playerForCurrentTurn = !ticTacToe.playerForCurrentTurn;
+      let piece = ticTacToe.playerForCurrentTurn ? ticTacToe.piece['1'] : ticTacToe.piece['0'];
+      ticTacToe.board[index] = cell.innerHTML = piece;
+      if (!ticTacToe.checkForWinner(index, piece)) {
+        ticTacToe.playerForCurrentTurn = !ticTacToe.playerForCurrentTurn;
+        ticTacToe.turnNumber++;
+      }  
     }
   },
   getIndexOfCell: function(id) {
